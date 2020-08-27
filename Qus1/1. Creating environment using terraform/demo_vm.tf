3d54a65ef211cba68b7eb345afaa7970fbba6511
Santhosh <santhovk5@gmail.com>
2020-08-27 18:37:49 +0530

    Updated Terraform Script

--- Qus1/1. Creating environment using terraform/demo_vm.tf
+++ Qus1/1. Creating environment using terraform/demo_vm.tf
@@ -1,47 +1,51 @@
-  # Configure the Microsoft Azure Provider
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-    subscription_id = "94030501-71f9-47c6-8b14-1fcc6428eba9"
+    version = "~>2.0"
+    features {}
 }
 
-resource "azurerm_resource_group" "myterraformgroup" {
-    name     = "myResourceGroup"
-    location = "eastus"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
+resource "azurerm_resource_group" "test" {
+ name     = "acctestrg"
+ location = "West US 2"
 }
 
-resource "azurerm_virtual_network" "myterraformnetwork" {
-    name                = "myVnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = "eastus"
-    resource_group_name = azurerm_resource_group.myterraformgroup.name
-
-    tags = {
-        environment = "Terraform Demo"
-    }
+resource "azurerm_virtual_network" "test" {
+ name                = "acctvn"
+ address_space       = ["10.0.0.0/16"]
+ location            = azurerm_resource_group.test.location
+ resource_group_name = azurerm_resource_group.test.name
 }
 
-resource "azurerm_subnet" "myterraformsubnet" {
-    name                 = "mySubnet"
-    resource_group_name  = azurerm_resource_group.myterraformgroup.name
-    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
-    address_prefix       = "10.0.1.0/24"
+resource "azurerm_subnet" "test" {
+ name                 = "acctsub"
+ resource_group_name  = azurerm_resource_group.test.name
+ virtual_network_name = azurerm_virtual_network.test.name
+ address_prefix       = "10.0.2.0/24"
 }
 
-
-resource "azurerm_public_ip" "myterraformpublicip" {
-    name                         = "myPublicIP"
-    location                     = "eastus"
-    resource_group_name          = azurerm_resource_group.myterraformgroup.name
-    allocation_method            = "Dynamic"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
+resource "azurerm_public_ip" "test" {
+ name                         = "publicIPForLB"
+ location                     = azurerm_resource_group.test.location
+ resource_group_name          = azurerm_resource_group.test.name
+ allocation_method            = "Static"
 }
 
+resource "azurerm_lb" "test" {
+ name                = "loadBalancer"
+ location            = azurerm_resource_group.test.location
+ resource_group_name = azurerm_resource_group.test.name
+
+ frontend_ip_configuration {
+   name                 = "publicIPAddress"
+   public_ip_address_id = azurerm_public_ip.test.id
+ }
+}
+
+resource "azurerm_lb_backend_address_pool" "test" {
+ resource_group_name = azurerm_resource_group.test.name
+ loadbalancer_id     = azurerm_lb.test.id
+ name                = "BackEndAddressPool"
+}
 
 resource "azurerm_network_interface" "test" {
  count               = 2
@@ -56,7 +60,6 @@ resource "azurerm_network_interface" "test" {
  }
 }
 
-
 resource "azurerm_managed_disk" "test" {
  count                = 2
  name                 = "datadisk_existing_${count.index}"
@@ -67,7 +70,6 @@ resource "azurerm_managed_disk" "test" {
  disk_size_gb         = "1023"
 }
 
-
 resource "azurerm_availability_set" "avset" {
  name                         = "avset"
  location                     = azurerm_resource_group.test.location
@@ -77,46 +79,63 @@ resource "azurerm_availability_set" "avset" {
  managed                      = true
 }
 
+resource "azurerm_virtual_machine" "test" {
+ count                 = 2
+ name                  = "acctvm${count.index}"
+ location              = azurerm_resource_group.test.location
+ availability_set_id   = azurerm_availability_set.avset.id
+ resource_group_name   = azurerm_resource_group.test.name
+ network_interface_ids = [element(azurerm_network_interface.test.*.id, count.index)]
+ vm_size               = "Standard_DS1_v2"
 
-# Create virtual machine
-resource "azurerm_virtual_machine" "myterraformvm" {
-	count                 = 2
-    name                  = "demo-vm${count.index}"
-    location              = "eastus"
-    resource_group_name   = azurerm_resource_group.myterraformgroup.name
-    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
-    vm_size               = "Standard_DS1_v2"
+ # Uncomment this line to delete the OS disk automatically when deleting the VM
+ # delete_os_disk_on_termination = true
 
-    storage_os_disk {
-        name              = "myOsDisk${count.index}"
-        caching           = "ReadWrite"
-        create_option     = "FromImage"
-        managed_disk_type = "Premium_LRS"
-    }
+ # Uncomment this line to delete the data disks automatically when deleting the VM
+ # delete_data_disks_on_termination = true
 
-    storage_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "16.04.0-LTS"
-        version   = "latest"
-    }
+ storage_image_reference {
+   publisher = "Canonical"
+   offer     = "UbuntuServer"
+   sku       = "16.04-LTS"
+   version   = "latest"
+ }
 
-    os_profile {
-        computer_name  = "hostname"
-        admin_username = "testadmin"
-	admin_password = "Password1234!"
-    }
+ storage_os_disk {
+   name              = "myosdisk${count.index}"
+   caching           = "ReadWrite"
+   create_option     = "FromImage"
+   managed_disk_type = "Standard_LRS"
+ }
 
-    os_profile_linux_config {
-       disable_password_authentication = false
-    }
+ # Optional data disks
+ storage_data_disk {
+   name              = "datadisk_new_${count.index}"
+   managed_disk_type = "Standard_LRS"
+   create_option     = "Empty"
+   lun               = 0
+   disk_size_gb      = "1023"
+ }
 
-    boot_diagnostics {
-        enabled = "true"
-        storage_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
-    }
+ storage_data_disk {
+   name            = element(azurerm_managed_disk.test.*.name, count.index)
+   managed_disk_id = element(azurerm_managed_disk.test.*.id, count.index)
+   create_option   = "Attach"
+   lun             = 1
+   disk_size_gb    = element(azurerm_managed_disk.test.*.disk_size_gb, count.index)
+ }
 
-    tags = {
-        environment = "Demo VM"
-    }
+ os_profile {
+   computer_name  = "hostname"
+   admin_username = "testadmin"
+   admin_password = "Password1234!"
+ }
+
+ os_profile_linux_config {
+   disable_password_authentication = false
+ }
+
+ tags = {
+   environment = "staging"
+ }
 }
